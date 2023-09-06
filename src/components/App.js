7e69365ee959eecbc737e1b5303d5daf658df419
anfdrva <angelina0403@ukr.net>
2023-09-06 17:40:00 +0300

    start

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export class App extends Component { 
+  
+}
+
+// export const App = () => {
+//   return (
+//     <div>
+//       React homework template
+//     </div>
+//   );
+// };
